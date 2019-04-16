e15670e1620181b713ce27f16ca9f3349179ef7c
ooftf <994749769@qq.com>
2019-04-17 01:46:30 +0800

    已经可以初步使用

--- lib/src/main/java/com/ooftf/homer/lib/IpcClient.java
+++ lib/src/main/java/com/ooftf/homer/lib/IpcClient.java
@@ -10,13 +10,20 @@ import io.reactivex.Single;
 public class IpcClient {
     public static Single<IpcResponseBody> request(Uri uri, IpcRequestBody body) {
         IClient client = IpcHostManager.getClientMap().get(uri.getHost());
+        if (!uri.getScheme().equals(IpcConst.IPC_SCHEME)) {
+            return Single.error(new IpcException("Uri scheme" + uri.getScheme() + " is not " + IpcConst.IPC_SCHEME, 502));
+        }
         if (client == null) {
             return Single.error(new IpcException("未找到 " + uri.getHost() + " 对应的AbsIpcClient", 502));
         }
         return client.call(uri, body);
     }
 
-    public static Uri.Builder getBaseUri(String host){
-        return  new Uri.Builder().scheme(IpcConst.IPC_SCHEME).authority(host);
+    public static Single<IpcResponseBody> request(Uri uri) {
+        return request(uri, null);
+    }
+
+    public static Uri.Builder getBaseUri(String host) {
+        return new Uri.Builder().scheme(IpcConst.IPC_SCHEME).authority(host);
     }
 }
